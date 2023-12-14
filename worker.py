fb03667d89e8e76b51519f96cae1c2304eb2adfc
DJ Drohan <77214928+DJDrohan@users.noreply.github.com>
2023-12-14 15:39:57 +0000

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,8 +1,8 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request
 import requests
 import os
 import json
+
 app = Flask(__name__)
 
 def get_api_key() -> str:
@@ -10,7 +10,7 @@ def get_api_key() -> str:
     if secret:
         return secret
     else:
-        #local testing
+        # Local testing
         with open('.key') as f:
             return f.read()
       
@@ -20,34 +20,34 @@ def hello():
 
 @app.route("/test")
 def test():
-    #return "Test" # testing 
-    return(get_api_key())
+    return get_api_key()
 
-@app.route("/add",methods=['GET','POST'])
+@app.route("/add", methods=['GET', 'POST'])
 def add():
-  if request.method=='GET':
-    return "Use post to add" # replace with form template
-  else:
-    token=get_api_key()
-    ret = addWorker(token,request.form['num'])
-    return ret
-
-
-def addWorker(token, num):
-    with open('payload.json') as p:
-      tdata=json.load(p)
-    tdata['name']='slave'+str(num)
-    data=json.dumps(tdata)
-    url='https://www.googleapis.com/compute/v1/projects/alien-emblem-399621/zones/europe-west1-b/instances'
-    headers={"Authorization": "Bearer "+token}
-    resp=requests.post(url,headers=headers, data=data)
-    if resp.status_code==200:     
-      return "Done"
+    if request.method == 'GET':
+        return "Use POST to add"  # Replace with form template
     else:
-      print(resp.content)
-      return "Error\n"+resp.content.decode('utf-8') + '\n\n\n'+data
+        token = get_api_key()
+        num = request.form.get('num')  # Safely get the 'num' value from the form
+        if not num:
+            return "Please provide 'num' parameter"
+        
+        ret = add_worker(token, num)
+        return ret
 
-
+def add_worker(token, num):
+    with open('payload.json') as p:
+        tdata = json.load(p)
+    tdata['name'] = 'slave' + str(num)
+    data = json.dumps(tdata)
+    url = 'https://www.googleapis.com/compute/v1/projects/alien-emblem-399621/zones/europe-west1-b/instances'
+    headers = {"Authorization": "Bearer " + token}
+    resp = requests.post(url, headers=headers, data=data)
+    if resp.status_code == 200:
+        return "Done"
+    else:
+        print(resp.content)
+        return "Error\n" + resp.content.decode('utf-8') + '\n\n\n' + data
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port='8080')
+    app.run(host='0.0.0.0', port=8080)
